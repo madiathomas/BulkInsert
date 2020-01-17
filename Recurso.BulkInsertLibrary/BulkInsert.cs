71a50bd290ba4ff414846459e949326aa7a8f72d
Thomas Madia <TMadia@lfmd.co.za>
2020-01-17 10:53:45 +0200

    Add project files.

---
+++ Recurso.BulkInsertLibrary/BulkInsert.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recurso.BulkInsertLibrary
+{
+    public class BulkInsert
+    {
+        /// <summary>
+        /// Save a generic list into a database table. If destinationTableName isn't supplied, it will use name of T as Table name.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        public async Task Save<T>(List<T> list, string connectionString, string destinationTableName = null)
+        {
+            using var dataTable = list.CopyToDataTable();
+
+            await Save(list, connectionString, destinationTableName);
+        }
+
+        /// <summary>
+        /// Save datatable into a table on the database. If destinationTableName isn't supplied, it will use name of T as Table name.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dataTable"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="destinationTableName"></param>
+        /// <returns></returns>
+        public async Task Save<T>(DataTable dataTable, string connectionString, string destinationTableName = null)
+        {
+            using SqlBulkCopy bulkCopy = new SqlBulkCopy(connectionString, SqlBulkCopyOptions.Default)
+            {
+                DestinationTableName = destinationTableName ?? dataTable.TableName
+            };
+
+            await bulkCopy.WriteToServerAsync(dataTable);
+        }
+
+    }
+}
---
+++ Recurso.BulkInsertLibrary/DataTableExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+
+namespace Recurso.BulkInsertLibrary
+{
+    public static class DataTableExtensions
+    {
+        /// <summary>
+        /// This method converts a generic list into a DataTable. Properties will be used as columns.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static DataTable CopyToDataTable<T>(this List<T> data)
+        {
+            var dataTable = new DataTable(nameof(T));
+
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(T));
+
+            foreach (PropertyDescriptor prop in properties)
+            {
+                dataTable.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            }
+
+            foreach (T item in data)
+            {
+                DataRow row = dataTable.NewRow();
+
+                foreach (PropertyDescriptor prop in properties)
+                {
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                }
+
+                dataTable.Rows.Add(row);
+            }
+
+            return dataTable;
+        }
+    }
+}
