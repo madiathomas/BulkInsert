e01235a6f00843c7fa4fd05459e343d3e04fa80d
Thomas Madia <TMadia@lfmd.co.za>
2020-01-20 10:46:10 +0200

    Added BatchSize to IBulkInsert to make BatchSize settable for those following SOLID principle

--- Recurso.BulkInsert/IBulkInsert.cs
+++ Recurso.BulkInsert/IBulkInsert.cs
@@ -7,10 +7,34 @@ namespace Recurso.BulkInsert
 {
     public interface IBulkInsert
     {
+        /// <summary>
+        /// Database connection string to be used to bulk insert data
+        /// </summary>
         public string ConnectionString { get; set; }
 
+        /// <summary>
+        /// Number of rows in each batch. At the end of each batch, the rows in the batch are sent to the server. Defaulted to 4000.
+        /// </summary>
+        public int BatchSize { get; set; }
+
+        /// <summary>
+        /// Save a generic list into a database table
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sourceList"></param>
+        /// <param name="destinationTableName">Name of the database table you want to bulk insert this data. If destinationTableName isn't supplied, it will use name of T as Table name.</param>
+        /// <param name="sqlBulkCopyOptions">Bitwise flag that specifies one or more options to use with an instance of SqlBulkCopy.</param>
+        /// <returns></returns>
         Task Save<T>(DataTable dataTable, string destinationTableName = null, SqlBulkCopyOptions sqlBulkCopyOptions = SqlBulkCopyOptions.Default);
 
+        /// <summary>
+        /// Save datatable into a table on the database
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sourceDataTable"></param>
+        /// <param name="destinationTableName">Name of the database table you want to bulk insert this data. If destinationTableName isn't supplied, it will use name of T as Table name.</param>
+        /// <param name="sqlBulkCopyOptions">Bitwise flag that specifies one or more options to use with an instance of SqlBulkCopy</param>
+        /// <returns></returns>
         Task Save<T>(List<T> list, string destinationTableName = null, SqlBulkCopyOptions sqlBulkCopyOptions = SqlBulkCopyOptions.Default);
     }
 }
